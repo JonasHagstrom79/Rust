e5823a184cc3b3fe2c6889ac6f981892779e3879
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2024-03-16 12:03:28 +0100

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    println!("Hello, world!");
+
+    let animals = ["Cat", "Dog", "Elephant", "Giraffe", "Lion", "Tiger", "Bear", "Fox", "Wolf", "Zebra"];
+    
+    for animal in animals.iter() {
+        println!("{}", animal);
+    }
+}
