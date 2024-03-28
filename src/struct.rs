5570d3918d4caa9bc8a55e12b33ecadef084a5ec
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2024-03-28 22:31:28 +0100

    impl

--- src/struct.rs
+++ src/struct.rs
@@ -8,6 +8,24 @@ struct Car {
     fuel_level: f32,
     price: u32,
 }
+// Defines methods associated with the Car struct
+impl Car {
+    // methods for Car struct
+    fn display_car_info(self) {
+        // self is the instance of the struct
+        println!("Owner: {}", self.owner);
+        println!("Year: {}", self.year);
+        println!("Fuel Level: {}", self.fuel_level);
+        println!("Price: {}", self.price);
+    }
+}
+
+fn display_car_info(car: &Car) {
+    println!("Owner: {}", car.owner);
+    println!("Year: {}", car.year);
+    println!("Fuel Level: {}", car.fuel_level);
+    println!("Price: {}", car.price);
+}
 
 fn main() {
     let my_car = Car {
@@ -35,4 +53,6 @@ fn main() {
     let point_1 = Point2D(1, 2);
     let point_2 = Point3D(1, 2, 3);
     let point_3 = Point4D(1, 2, 3, 4);
+
+    my_car.display_car_info();
 }
