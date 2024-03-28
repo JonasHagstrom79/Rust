39e9284d932cc6c582ff70b61f09caa7b9b6ff6e
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2024-03-28 22:23:10 +0100

    Tuple structs

--- src/struct.rs
+++ src/struct.rs
@@ -9,4 +9,30 @@ struct Car {
     price: u32,
 }
 
-fn main() {}
+fn main() {
+    let my_car = Car {
+        owner: String::from("John Doe"),
+        year: 2010,
+        fuel_level: 0.5,
+        price: 10000,
+    };
+    let car_year = my_car.year;
+    let another_car = Car {
+        owner: String::from("Jane Doe"),
+        ..my_car // copy the rest of the fields from my_car
+    };
+    println!("Car year: {}", car_year);
+
+    // Tuple struct
+    let point_2D = (1, 2);
+    let point_3D = (1, 2, 3);
+    let point_4D = (1, 2, 3, 4);
+
+    struct Point2D(i32, i32);
+    struct Point3D(i32, i32, i32);
+    struct Point4D(i32, i32, i32, i32);
+
+    let point_1 = Point2D(1, 2);
+    let point_2 = Point3D(1, 2, 3);
+    let point_3 = Point4D(1, 2, 3, 4);
+}
